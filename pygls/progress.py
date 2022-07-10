8adc050ff3ac8965b0c2837f1ad016114402d67f
Jérome Perrin <perrinjerome@gmail.com>
2022-07-10 21:03:44 +0900

    progress: support cancellation from client
    This allows servers to react when client cancel requests, either by
    polling is_cancelled or by registering a callback using on_cancel.
    
    The old cancel method is removed, because it was not corresponding to
    anything from the spec.
    
    Note that the tests have to be skipped on pyodide, as they need
    asyncio.sleep for a delay non blocking message processing.

--- pygls/progress.py
+++ pygls/progress.py
@@ -1,13 +1,21 @@
 import asyncio
 from concurrent.futures import Future
-from typing import Dict
+import enum
+from typing import Callable, Dict
 
-from pygls.lsp.methods import (PROGRESS_NOTIFICATION, WINDOW_WORK_DONE_PROGRESS_CANCEL,
-                               WINDOW_WORK_DONE_PROGRESS_CREATE)
+from pygls.lsp.methods import (PROGRESS_NOTIFICATION, WINDOW_WORK_DONE_PROGRESS_CREATE)
 from pygls.lsp.types.basic_structures import (ProgressParams, ProgressToken, WorkDoneProgressBegin,
                                               WorkDoneProgressEnd, WorkDoneProgressReport)
 from pygls.lsp.types.window import WorkDoneProgressCancelParams, WorkDoneProgressCreateParams
 from pygls.protocol import LanguageServerProtocol
+
+
+class _ProgressTokenStatus(enum.Enum):
+    """The status of a work done progress
+    """
+    Running = 0
+    Cancelled = 1
+    Done = 2
 
 
 class Progress:
@@ -16,22 +24,26 @@ class Progress:
     Attributes:
         _lsp(LanguageServerProtocol): Language server protocol instance
         tokens(dict): Holds progress bar tokens that are already registered
+        tokens_cancel_callbacks(dict): Holds cancel callbacks if registered
     """
 
     def __init__(self, lsp: LanguageServerProtocol) -> None:
         self._lsp = lsp
 
-        self.tokens: Dict[ProgressToken, None] = {}
+        self.tokens: Dict[ProgressToken, _ProgressTokenStatus] = {}
+        self.tokens_cancel_callbacks: Dict[ProgressToken, Callable[[], None]] = {}
 
     def _check_token_registered(self, token: ProgressToken) -> None:
         if token in self.tokens:
             raise Exception("Token is already registered!")
 
     def create(self, token: ProgressToken, callback=None) -> Future:
+        """Create a server initiated work done progress.
+        """
         self._check_token_registered(token)
 
         def on_created(*args, **kwargs):
-            self.tokens[token] = None
+            self.tokens[token] = _ProgressTokenStatus.Running
             if callback is not None:
                 callback(*args, **kwargs)
 
@@ -42,33 +54,41 @@ class Progress:
         )
 
     async def create_async(self, token: ProgressToken) -> asyncio.Future:
+        """Create a server initiated work done progress.
+        """
         self._check_token_registered(token)
 
         result = await self._lsp.send_request_async(
             WINDOW_WORK_DONE_PROGRESS_CREATE,
             WorkDoneProgressCreateParams(token=token),
         )
-        self.tokens[token] = None
+        self.tokens[token] = _ProgressTokenStatus.Running
 
         return result
 
-    def cancel(self, token: ProgressToken, callback=None) -> Future:
-        def on_canceled(*args, **kwargs):
-            del self.tokens[token]
+    def cancel_notification_received(self, params: WorkDoneProgressCancelParams) -> None:
+        """Called by pgls when a cancel notification is received from client.
+        """
+        callback = self.tokens_cancel_callbacks.get(params.token)
+        if callback:
+            callback()
+        self.tokens[params.token] = _ProgressTokenStatus.Cancelled
 
-            if callback is not None:
-                callback(*args, **kwargs)
+    def is_cancelled(self, token: ProgressToken) -> bool:
+        """Check if work done progress was cancelled.
+        """
+        return self.tokens.get(token) == _ProgressTokenStatus.Cancelled
 
-        return self._lsp.send_request(
-            WINDOW_WORK_DONE_PROGRESS_CANCEL,
-            WorkDoneProgressCancelParams(token=token),
-            callback=on_canceled,
-        )
-
-    def cancel_async(self, token: ProgressToken) -> asyncio.Future:
-        return asyncio.wrap_future(self.cancel(token))
+    def on_cancel(self, token: ProgressToken, cancel_callback: Callable[[], None]) -> None:
+        """Register a callback to be called if the if work done progress is cancelled.
+        """
+        if token in self.tokens_cancel_callbacks:
+            raise ValueError(f'Cancel callback for {token} already registered.')
+        self.tokens_cancel_callbacks[token] = cancel_callback
 
     def begin(self, token: ProgressToken, value: WorkDoneProgressBegin) -> None:
+        """Notify beginning of work.
+        """
         return self._lsp.notify(
             PROGRESS_NOTIFICATION,
             ProgressParams(
@@ -78,7 +98,13 @@ class Progress:
         )
 
     def report(self, token: ProgressToken, value: WorkDoneProgressReport) -> None:
+        """Notify progress of work.
+        """
         self._lsp.notify(PROGRESS_NOTIFICATION, ProgressParams(token=token, value=value))
 
     def end(self, token: ProgressToken, value: WorkDoneProgressEnd) -> None:
+        """Notify end of work.
+        """
         self._lsp.notify(PROGRESS_NOTIFICATION, ProgressParams(token=token, value=value))
+        self.tokens[token] = _ProgressTokenStatus.Done
+        self.tokens_cancel_callbacks.pop(token, None)
--- tests/lsp/test_progress.py
+++ tests/lsp/test_progress.py
@@ -15,9 +15,13 @@
 # limitations under the License.                                           #
 ############################################################################
 
+import asyncio
 from typing import List, Optional
 
-from pygls.lsp.methods import CODE_LENS, PROGRESS_NOTIFICATION
+import pytest
+
+from pygls import IS_PYODIDE
+from pygls.lsp.methods import CODE_LENS, PROGRESS_NOTIFICATION, WINDOW_WORK_DONE_PROGRESS_CANCEL
 from pygls.lsp.types import (
     CodeLens,
     CodeLensParams,
@@ -28,8 +32,8 @@ from pygls.lsp.types import (
     WorkDoneProgressReport,
 )
 from pygls.lsp.types.basic_structures import ProgressParams
+from pygls.lsp.types.window import WorkDoneProgressCancelParams
 from ..conftest import ClientServer
-
 
 class ConfiguredLS(ClientServer):
     def __init__(self):
@@ -41,23 +45,39 @@ class ConfiguredLS(ClientServer):
             CodeLensOptions(resolve_provider=False,
                             work_done_progress=True),
         )
-        def f1(params: CodeLensParams) -> Optional[List[CodeLens]]:
+        async def f1(params: CodeLensParams) -> Optional[List[CodeLens]]:
             self.server.lsp.progress.begin(
                 params.work_done_token, WorkDoneProgressBegin(
                     title="starting", percentage=0)
             )
-            self.server.lsp.progress.report(
-                params.work_done_token,
-                WorkDoneProgressReport(message="doing", percentage=50),
-            )
-            self.server.lsp.progress.end(
-                params.work_done_token, WorkDoneProgressEnd(message="done")
-            )
+            def cancelled():
+                self.server.lsp.progress.report(
+                    params.work_done_token, WorkDoneProgressReport(message="cancel callback")
+                )
+            self.server.lsp.progress.on_cancel(params.work_done_token, cancelled)
+            await asyncio.sleep(0.1)
+            if self.server.lsp.progress.is_cancelled(params.work_done_token):
+                self.server.lsp.progress.end(
+                    params.work_done_token, WorkDoneProgressEnd(message="cancelled")
+                )
+            else:
+                self.server.lsp.progress.report(
+                    params.work_done_token,
+                    WorkDoneProgressReport(message="doing", percentage=50),
+                )
+                self.server.lsp.progress.end(
+                    params.work_done_token, WorkDoneProgressEnd(message="done")
+                )
             return None
 
         @self.client.feature(PROGRESS_NOTIFICATION)
         def f2(params):
             self.client.notifications.append(params)
+            if params.value['kind'] == 'begin' and params.token == "token_with_cancellation":
+                # client cancels the progress token
+                self.client.lsp.notify(
+                    WINDOW_WORK_DONE_PROGRESS_CANCEL,
+                    WorkDoneProgressCancelParams(token=params.token))
 
 
 @ConfiguredLS.decorate()
@@ -70,6 +90,7 @@ def test_capabilities(client_server):
     assert provider.work_done_progress is True
 
 
+@pytest.mark.skipif(IS_PYODIDE, reason='threads are not available in pyodide.')
 @ConfiguredLS.decorate()
 def test_progress_notifications(client_server):
     client, _ = client_server
@@ -97,3 +118,32 @@ def test_progress_notifications(client_server):
     assert client.notifications[2].token == 'token'
     assert client.notifications[2].value == {
         "kind": "end", "message": "done"}
+
+
+@pytest.mark.skipif(IS_PYODIDE, reason='threads are not available in pyodide.')
+@ConfiguredLS.decorate()
+def test_progress_cancellation(client_server):
+    client, _ = client_server
+    client.lsp.send_request(
+        CODE_LENS,
+        CodeLensParams(
+            text_document=TextDocumentIdentifier(uri="file://return.none"),
+            work_done_token='token_with_cancellation',
+        ),
+    ).result()
+
+    assert len(client.notifications) == 3
+    assert client.notifications[0].token == 'token_with_cancellation'
+    assert client.notifications[0].value == {
+        "kind": "begin",
+        "title": "starting",
+        "percentage": 0,
+    }
+    assert client.notifications[1].token == 'token_with_cancellation'
+    assert client.notifications[1].value == {
+        "kind": "report",
+        "message": "cancel callback",
+    }
+    assert client.notifications[2].token == 'token_with_cancellation'
+    assert client.notifications[2].value == {
+        "kind": "end", "message": "cancelled"}
